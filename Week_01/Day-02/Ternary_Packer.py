f6d68370c6e690a71139743da97991bf518c7d96
Jobaer Islam <156295576+Jobaer-Islam@users.noreply.github.com>
2025-12-16 17:51:36 +0600

    Ternary

---
+++ Week_01/Day-02/Ternary_Packer.py
@@ -0,0 +1,13 @@
+score = 85
+
+# The Ternary Expression
+status = "Pass" if score > 70 else "Fail"
+
+print(f"Score: {score}, Status: {status}")
+
+score = 65
+
+# The Ternary Expression again
+status = "Pass" if score > 70 else "Fail"
+
+print(f"Score: {score}, Status: {status}")
