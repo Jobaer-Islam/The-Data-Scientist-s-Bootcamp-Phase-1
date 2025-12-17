cb0c2c06bf76f465efe7f4b7399467afa3806fcf
Jobaer Islam <156295576+Jobaer-Islam@users.noreply.github.com>
2025-12-17 13:06:18 +0600

    Identity Swap

--- Week_01/Day - 01/Identity_Swap.py
+++ Week_01/Day - 01/Identity_Swap.py
@@ -6,3 +6,6 @@ left_hand, right_hand = right_hand, left_hand
 
 print(f"Left Hand: {left_hand}")   # Tea
 print(f"Right Hand: {right_hand}") # Coffee
+
+Why it works: Python creates a hidden "package" (a tuple) containing (right_hand, left_hand) first,
+then opens that package and assigns the contents to the names on the left side.
